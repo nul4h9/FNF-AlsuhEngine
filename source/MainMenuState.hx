efd62cbe6d2e2152fcabf8b496f3e6e29683e31c
Null <alansurtaev2008@mail.ru>
2025-01-03 22:38:18 +0700

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -42,7 +42,7 @@ class MainMenuState extends MusicBeatState
 	var magenta:Sprite;
 	var camFollow:FlxObject;
 
-	public static var alsuhEngineVersion:String = '0.1.2h';
+	public static var alsuhEngineVersion:String = '0.2-prerelease';
 
 	override function create():Void
 	{
